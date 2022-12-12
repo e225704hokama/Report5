e4083a8647f255fc7f3892ac71ad3ffd87788ac0
Hokama Reo <e225704@ie.u-ryukyu.ac.jp>
2022-12-12 19:42:24 +0900

    step3

--- src/NullError.java
+++ src/NullError.java
@@ -1,15 +1,8 @@
 public class NullError{
     public static void main(String[] args) {
-        String str;
-
-        str = null;
-
-        try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-          System.out.println("NullPointerExceptionが発生しました。"); 
-          System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        
         
     }
 }
